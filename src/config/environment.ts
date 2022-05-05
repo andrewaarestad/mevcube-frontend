b01e9334eef073f991b1d43bc6c801f8187d0bec
Andrew Aarestad <andrew@aa-engineering.com>
2022-05-04 23:49:47 -0500

    add useRef hook to prevent infinte loop with dependency

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -11,7 +11,7 @@ interface IEnvironment {
 const hhEnvironment = {
   RPC: 'http://localhost:8545',
   BlockExplorer: 'http://localhost:4444',
-  MevCubeContractAddress: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+  MevCubeContractAddress: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
   ChainId: 1337,
   ChainName: 'Local Hardhat Network'
 };
--- src/containers/app/wallet-connected-cube.tsx
+++ src/containers/app/wallet-connected-cube.tsx
@@ -1,6 +1,6 @@
 import {CubeLoading} from "./cube-loading";
 import {CubeLoaded} from "./cube-loaded";
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import {useTypedSelector} from "../../store/reducers";
 import {useAppDispatch} from "../../store";
 import {MessagesService} from "../../services/messages-service";
@@ -11,6 +11,7 @@ export const WalletConnectedCube = () => {
   const dispatch = useAppDispatch();
   const {flags: {isLoadingInitialCubeContractState}, contractStateIsSolved, currentScrambleRewardHex} = useTypedSelector(state => state.cube);
 
+  const currentScrambleRewardHexRef = useRef(currentScrambleRewardHex);
   // const suggestScramble = useCallback(() => {
   //   if (contractStateIsSolved) {
   //     callbacks.register(messageId, () => {
@@ -22,7 +23,7 @@ export const WalletConnectedCube = () => {
   useEffect(() => {
     if (!isLoadingInitialCubeContractState && contractStateIsSolved) {
       console.log('done loading initial contract state');
-      MessagesService.createMessage(dispatch, 'Scramble the cube!', 'The cube is currently solved.  Why don\'t you try scrambling it? The reward for scrambling is currently ' + ethers.utils.parseEther(new ethers.utils.BigNumber(currentScrambleRewardHex).toString()).toString());
+      MessagesService.createMessage(dispatch, 'Scramble the cube!', 'The cube is currently solved.  Why don\'t you try scrambling it? The reward for scrambling is currently ' + ethers.utils.parseEther(new ethers.utils.BigNumber(currentScrambleRewardHexRef.current).toString()).toString());
       // suggestScramble();
     }
   }, [isLoadingInitialCubeContractState, contractStateIsSolved, dispatch]);
